d80df260092ac3a4e30f0cfcf36a86841d2555c5
vorop_000 <voropaev.p@gmail.com>
2015-01-31 07:29:27 +0400

    1

--- client_side_scripts/jobsdb.py
+++ client_side_scripts/jobsdb.py
@@ -3,85 +3,108 @@ __author__ = 'Pavel Voropaev'
 import sys
 import codecs
 import json
+import cPickle
+from collections import OrderedDict
+
 import unicodecsv
 
 
-class jobsdb:
-    def __init__(self, path, type_):
-        self.path = path
-        self.content = dict()
-        if type_ == "json":
-            self._read_json()
-        if type_ == "csv":
-            self._read_csv()
-        else:
-            return
+class JobsDB(object):
+    NUMBER_OF_FILES = 31  # absolute field offset for file count in csv or in content object
+    ATTEMPTS = 32  # absolute field offset for job attempts in content
+    NUMBER_OF_LOGS = 9  # field offset relative to attempt with log file count in csv
+    NUMBER_OF_FIELDS = 57  # total minimum number of fields in content, might be more due to database backups
+
+    def __init__(self, content):
+        assert type(content) is OrderedDict, "type is not OrderedDict for content: %r" % type(content)
+        self.content = content
 
     def __getitem__(self, job_id):
-        if job_id in self.content:
-            return self.content[job_id]
+        if job_id in self:
+            return self[job_id]
         else:
             return None
 
     def __iter__(self):
         if self.content:
-            for content in self.content:
+            for content in self:
                 yield content
 
-    def _read_csv(self):
-        csvfile = open(self.path)
+    def __len__(self):
+        if self.content:
+            return len(self.content)
+        else:
+            return 0
+
+    @classmethod
+    def from_csv(cls, path):
+        csvfile = codecs.open(path)
+        content = OrderedDict()
         csv_reader = unicodecsv.reader(csvfile, delimiter=',', escapechar='\\', encoding='utf-8', errors='ignore')
         for line in csv_reader:
-            self.content[line[0]] = list()
-            for i in range(1, 31):
-                self.content[line[0]].append(line[i])
-            offset = int(line[31])  # Number of files
+            content[line[0]] = list()
+            for i in range(1, cls.NUMBER_OF_FILES):
+                content[line[0]].append(line[i])
+            offset = int(line[cls.NUMBER_OF_FILES])  # Number of files
             if offset in (0, None):
-                filelist = [None]
+                file_list = [None]
             else:
-                filelist = line[32:32 + offset]  # Filenames
-            self.content[line[0]].append(filelist)
-            attempts = list()
-            if line[33 + offset] == 0:
+                file_list = line[cls.NUMBER_OF_FILES + 1:cls.NUMBER_OF_FILES + 2 + offset]  # File names
+            content[line[0]].append(file_list)
+            attempts = dict()
+            if line[cls.NUMBER_OF_FILES + 2 + offset] == 0:
                 attempts = [None]
             else:
-                offset += 32
+                offset += cls.NUMBER_OF_FILES + 1
                 attempts_number = int(line[offset])
                 for i in range(1, attempts_number + 1):
-                    attempt = line[1 + offset:9 + offset]
-                    log_offset = int(line[9 + offset])
+                    attempt = line[1 + offset:cls.NUMBER_OF_LOGS + offset]
+                    log_offset = int(line[cls.NUMBER_OF_LOGS + offset])
                     if log_offset in (0, None):
                         logs = [None]
                         log_offset = 1
                     else:
-                        logs = line[10 + offset:10 + log_offset + offset]
+                        logs = line[cls.NUMBER_OF_LOGS + 1 + offset:cls.NUMBER_OF_LOGS + 1 + log_offset + offset]
                     offset += log_offset
                     attempt.append(logs)
-                    attempt.append(line[10 + offset])  # trybyteswritten
-                    attempt.append(line[11 + offset])  # tryfileswritten
-                    attempts.append(attempt)
+                    attempt.append(line[cls.NUMBER_OF_LOGS + 1 + offset])  # trybyteswritten
+                    attempt.append(line[cls.NUMBER_OF_LOGS + 2 + offset])  # tryfileswritten
+                    attempts[i] = attempt
                     offset += 11
-            self.content[line[0]].append(attempts)
+            content[line[0]].append(attempts)
             for i in range(offset + 1, len(line) - 1):
-                self.content[line[0]].append(line[i])
-            if len(self.content[line[0]]) == 57:  # padding with None
-                self.content[line[0]].append(None)
-                self.content[line[0]].append(None)
-            if len(self.content[line[0]]) == 58:
-                self.content[line[0]].append(None)
-            if len(self.content[line[0]]) != 59:
-                return 1
+                content[line[0]].append(line[i])
+            if len(content[line[0]]) == cls.NUMBER_OF_FIELDS:  # padding with None
+                content[line[0]].append(None)
+                content[line[0]].append(None)
+            if len(content[line[0]]) == cls.NUMBER_OF_FIELDS + 1:
+                content[line[0]].append(None)
+            if len(content[line[0]]) != cls.NUMBER_OF_FIELDS + 2:
+                raise Exception("Could not parse following line" + line)
         csvfile.close()
+        items = content.items()
+        items.reverse()
+        content = OrderedDict(items)
+        return cls(content)
 
-    def _read_json(self):
-        jsonfile = open(self.path)
-        self.content = json.load(jsonfile)
+    @classmethod
+    def from_json(cls, path):
+        jsonfile = open(path)
+        content = OrderedDict(json.load(jsonfile))
         jsonfile.close()
+        return cls(content)
+
+    def get_active_jobs(self):
+        active_jobs = list()
+        for job_id in self.content:
+            if self.content[job_id][3] == u'3':
+                active_jobs.append(job_id)
+        return active_jobs
 
     def write_json(self, path):
         try:
             jsonfile = codecs.open(path, 'w', encoding='utf-8')
-            self.content = json.dump(self.content, jsonfile)
+            self.content = json.dump(self, jsonfile)
             jsonfile.close()
         except IOError:
             print u"Couldn't write file at {0:s}".format(path)
@@ -90,7 +113,57 @@ class jobsdb:
             raise
 
 
-data = jobsdb(r"C:\Users\vorop_000\Desktop\all_columns", "csv")
-print len(data.content)
-data.write_json(r"C:\Users\vorop_000\Desktop\all_columns.json")
-pass
+class JobsDBDiff(object):
+    def __init__(self, jobs_db_old, jobs_db_new):
+        assert type(jobs_db_new) is JobsDB, "Type is not JobsDB for jobs_db_new: %r" % type(jobs_db_new)
+        assert type(jobs_db_old) is JobsDB, "Type is not JobsDB for jobs_db_old: %r" % type(jobs_db_old)
+        self.content_diff = OrderedDict()
+        self.attempts_diff = OrderedDict()
+        self.file_list_diff = OrderedDict()
+        if jobs_db_new.content.keys()[1] < jobs_db_old.content.keys()[1]:
+            raise Exception("New jobs object appears to be older the old jobs object" + jobs_db_new.keys()[
+                1] + "should be greater than" + jobs_db_old.keys()[1])
+
+        while True:
+            if len(jobs_db_new) == 0:
+                break
+            new_job_id = jobs_db_new.content.keys()[1]
+            old_job_id = jobs_db_old.content.keys()[1]
+            if new_job_id > old_job_id:
+                self.content_diff[new_job_id] = jobs_db_new.content[new_job_id]
+                del jobs_db_new.content.keys()[1]
+            else:
+                break
+
+        active_jobs = jobs_db_old.get_active_jobs()
+        for job_id in active_jobs:
+            if jobs_id not in jobs_db_new:
+                self.content_diff[job_id] = jobs_db_new[job_id]
+            else:
+                if jobs_db_new[job_id][:32] != jobs_db_old[job_id]:
+                    self.content_diff[job_id] = jobs_db_new[job_id]
+                else:
+                    self.file_list_diff[job_id] = set(
+                        jobs_db_new[job_id][cls.NUMBER_OF_FILES]) - set(jobs_db_old[job_id][cls.NUMBER_OF_FILES])
+                    self.attempts[job_id] = [job_id] = jobs_db_new[job_id][set(
+                        jobs_db_new[job_id][cls.ATTEMPTS]) - set(jobs_db_old[job_id][cls.ATTEMPTS])]
+                    if len(self.file_list_diff[job_id]) == 0:
+                        del self.file_list_diff[job_id]
+                    for attempt in jobs_db_new[job_id][cls.ATTEMPTS]:
+                        self.attempts[job_id][attempt] = OrderedDict()
+                        if attempt not in jobs_db_old[job_id][cls.ATTEMPTS]:
+                            self.attempts[job_id][attempt] = jobs_db_new[job_id][cls.ATTEMPTS][attempt]
+                        else:
+                            file_list = set(obs_db_new[job_id][cls.ATTEMPTS][attempt]) - set(
+                                obs_db_old[job_id][cls.ATTEMPTS][attempt])
+                            self.attempts[job_id][attempt] = list(file_list)
+
+
+data1 = JobsDB.from_csv(r"C:\Users\vorop_000\Desktop\all_columns")
+data = JobsDB.from_json(r"C:\Users\vorop_000\Desktop\all_columns.json")
+data_diff = JobsDBDiff(data,data1)
+print data_diff.content_diff
+print len(data)
+print len(data1)
+
+# data.write_json(r"C:\Users\vorop_000\Desktop\all_columns.json")
